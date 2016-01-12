011dfde4ed8ceffb5face95e5c1b9b4d0d0555a6
Dave Wyatt <dlwyatt115@gmail.com>
2016-01-12 11:24:03 -0500

    Adding repro script

---
+++ __Repro.ps1
@@ -0,0 +1,18 @@
+﻿$configData = @{
+    AllNodes = @(
+        @{
+            NodeName = '*'
+            SomeOtherProperty = 'Set me here'
+        }
+    )
+}
+
+configuration Something
+{
+    node someNode
+    {
+        Write-Verbose "Some Other Property: $($Node.SomeOtherProperty)" -Verbose
+    }
+}
+
+Something -OutputPath $env:temp\MOF  -ConfigurationData $configData | Out-Null
